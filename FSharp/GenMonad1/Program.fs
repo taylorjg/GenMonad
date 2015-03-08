4d53b00f73f92a6545788581606de0c4af8346ce
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-03-08 20:47:39 +0000

    Added simple F# example

---
+++ FSharp/GenMonad1/Program.fs
@@ -0,0 +1,13 @@
+﻿open FsCheck
+open Gen
+open System
+
+[<EntryPoint>]
+let main _ = 
+    let g = gen {
+        let! s = Arb.generate<string> |> suchThat (String.IsNullOrEmpty >> not)
+        let! c = elements s
+        return (s, c)
+    }
+    sample 50 10 g |> Seq.iter (printfn "%A")
+    0
