677842bc17422b0e8e4ee1ad1204d4547c92df3f
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-03-08 09:29:08 +0000

    Converted tabs to spaces

--- Haskell/GenMonad2.hs
+++ Haskell/GenMonad2.hs
@@ -2,7 +2,7 @@ import Test.QuickCheck
 
 main :: IO ()
 main =
-	do
-		let g = (arbitrary :: Gen String) `suchThat` (not . null) >>= \s ->
-			(\c -> (s, c)) `fmap` (elements s)
-		sample g
+    do
+        let g = (arbitrary :: Gen String) `suchThat` (not . null) >>= \s ->
+            (\c -> (s, c)) `fmap` (elements s)
+        sample g
