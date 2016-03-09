a06473b2897e065a36c26c6b92a267d0483bcfa4
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-03-09 10:29:14 +0000

    Addition to the F# version of the code showing bind/map as well as than bind/bind

--- FSharp/GenMonad1/Program.fs
+++ FSharp/GenMonad1/Program.fs
@@ -4,10 +4,20 @@ open System
 
 [<EntryPoint>]
 let main _ = 
-    let g = gen {
+
+    let g1 = gen {
         let! s = Arb.generate<string> |> suchThat (String.IsNullOrEmpty >> not)
         let! c = elements s
         return (s, c)
     }
-    sample 50 10 g |> Seq.iter (printfn "%A")
+    sample 50 10 g1 |> Seq.iter (printfn "%A")
+
+    printfn "%s" (new string('-', 80))
+
+    let g2 = gen {
+        let! s = Arb.generate<string> |> suchThat (String.IsNullOrEmpty >> not)
+        return! elements s |> map (fun c -> (s, c))
+    }
+    sample 50 10 g2 |> Seq.iter (printfn "%A")
+
     0
